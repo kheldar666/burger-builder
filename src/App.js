1dfb6aca3ea14fad1f035a0ab006920f366e0665
Martin Papy <martin.papy@gmail.com>
2020-05-04 16:55:31 +0700

    Basic Layout Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
+
 
 function App() {
   return (
-    <div>
-    </div>
+    <Layout>
+      <p>Test</p>
+    </Layout>
   );
 }
 
