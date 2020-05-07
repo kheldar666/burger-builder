86b84e4a419901b7659d51039d269d3a4eaa2516
Martin Papy <martin.papy@gmail.com>
2020-05-07 15:59:53 +0700

    Creation of the Nav component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Wrapper from '../../hoc/Wrapper';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 import classes from './Layout.module.css';
 
 const layout = ( props ) => {
     return(
         <Wrapper>
-            <div>Toolbar, SideDrawer, BackDrop</div>
+            <Toolbar />
             <main className={classes.Content} >
                 {props.children}
             </main>
