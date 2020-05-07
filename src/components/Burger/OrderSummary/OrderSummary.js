914d7d3a4913b8eda06f3074ca398a6331b0fa46
Martin Papy <martin.papy@gmail.com>
2020-05-07 15:26:26 +0700

    Adding Button components

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Wrapper from "../../../hoc/Wrapper";
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -19,6 +20,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to Checkout ?</p>
+            <Button btnType="Danger" click={props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" click={props.purchaseContinued}>CONTINUE</Button>
         </Wrapper>
     );
 
